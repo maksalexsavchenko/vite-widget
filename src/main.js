2904c0c68b3c499d058f9993189328bc92a90766
maksalexsavchenko <makskidexom@gmail.com>
2023-06-01 18:01:22 +0300

    fixes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import WidgetComponent from "./components/WidgetComponent.vue";
+import {createRouter, createWebHistory} from "vue-router";
 
-createApp(App).mount('#root');
+const routes = [
+    {
+        name: 'Home',
+        path: '/',
+        component: WidgetComponent
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+
+const app = createApp(App);
+
+app.use(router)
+
+app.mount('#root');
